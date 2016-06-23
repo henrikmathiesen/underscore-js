e4f91c9369c4ab0c70a67748a3f1ebd796930b49
Henrik <henrik_mathiesen@hotmail.com>
2016-06-23 20:39:54 +0200

    -tests

--- tst/03-arrays/03-arrays-flatten.test.js
+++ tst/03-arrays/03-arrays-flatten.test.js
@@ -10,4 +10,24 @@ Flattens a nested array (the nesting can be to any depth). If you pass shallow, 
 
 describe("03-arrays-flatten", function () {
     
+    it("should flatten an array", function () {
+        
+        var myArray = [
+            "Adam",
+            [1,2,3],
+            [
+                1,
+                [1,2]
+            ]
+        ];
+
+        var myArrayFlattened = _.flatten(myArray);
+        var myArrayFlattenedShallow = _.flatten(myArray, true);
+
+        expect(myArrayFlattened.length).toBe(7);
+        expect(myArrayFlattenedShallow.length).toBe(6);
+
+    });
+
+
 });
