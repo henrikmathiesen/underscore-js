9a30c1796a92e91abce8396af664b46709af4619
Henrik <henrik_mathiesen@hotmail.com>
2016-06-24 21:15:09 +0200

    -compose

--- app/04-functions/04-functions-compose.js
+++ app/04-functions/04-functions-compose.js
@@ -2,6 +2,23 @@
 
 (function(){
 
+    var functionOne = function(){
+        console.log(arguments);
+        return "fnOne";
+    };
 
+    var functionTwo = function(){
+        console.log(arguments);
+        return "fnTwo";
+    };
+
+    var functionThree = function(){
+        console.log(arguments);
+        return "fnThree";
+    };
+
+    // functionThree will be run first
+    var composed = _.compose(functionOne, functionTwo, functionThree);
+    composed("Foo");
 
 })();
