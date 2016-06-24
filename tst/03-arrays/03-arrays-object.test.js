f3637a3b66bd16ef88b3ce4947d7daa8ab03d8d1
Henrik <henrik_mathiesen@hotmail.com>
2016-06-24 08:24:53 +0200

    tests

--- tst/03-arrays/03-arrays-object.test.js
+++ tst/03-arrays/03-arrays-object.test.js
@@ -9,5 +9,26 @@ Converts arrays into objects. Pass either a single list of [key, value] pairs, o
 */
 
 describe("03-arrays-object", function () {
-    
+
+    it("should create an object from an array A", function () {
+
+        var myArrayKeys = [1, 2, 3];
+        var myArray = ["Adam", "Bertil", "Ceasar"];
+        var myObj = _.object(myArrayKeys, myArray);
+
+        expect(myObj).toEqual({ 1: "Adam", 2: "Bertil", 3: "Ceasar" });
+
+    });
+
+    it("should create an object from an array B", function () {
+        var myArrayKeyVal = [
+            ["firstName", "Adam"],
+            ["lastName", "Bertilsson"]
+        ];
+
+        var myObj = _.object(myArrayKeyVal);
+
+        expect(myObj).toEqual({ firstName: "Adam", lastName: "Bertilsson" });
+    });
+
 });
