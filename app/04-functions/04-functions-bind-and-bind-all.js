5fc649f4d97e7101fbc98e3412057a8f1357e7ba
Henrik <henrik_mathiesen@hotmail.com>
2016-06-24 22:03:51 +0200

    -bind

--- app/04-functions/04-functions-bind-and-bind-all.js
+++ app/04-functions/04-functions-bind-and-bind-all.js
@@ -2,6 +2,32 @@
 
 (function(){
 
+    // this points to window
+    console.log(this);
 
+    $('[data-click-me]').on('click', function(ev){
+        // this points to the element clicked (the same as ev.target)
+        console.log(this);
+        console.log(ev.target);
+    });
+
+    var vm = {
+        foo: "bar",
+        sayFoo: function(){
+            console.log(this.foo);
+        }
+    };
+
+    // this points correctly to the object when called from this function
+    vm.sayFoo();
+
+    //_.bindAll(vm, ['sayFoo']);
+
+    // The problem in the sayFoo function, when called from the click handler, is that this points to the element and not to the object, so foo is undefined
+    // That can be remedid by _.bindAll
+    //$('[data-click-me-b]').on('click', vm.sayFoo);
+    
+    // or by _.bind, like here
+    $('[data-click-me-b]').on('click', _.bind(vm.sayFoo, vm));
 
 })();
