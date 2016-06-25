2ce8d87ad940e227bc122aa5ca1e8800e2bd847d
Henrik <henrik_mathiesen@hotmail.com>
2016-06-25 11:19:48 +0200

    -memoize

--- app/04-functions/04-functions-memoize.js
+++ app/04-functions/04-functions-memoize.js
@@ -1,7 +1,21 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
-(function(){
+(function () {
 
 
+    var getNumber = function(){
+        return 9999999999999 * 9999999999999999;
+    };
+
+    var logNumber = function () {
+        console.time("getNumber");
+        console.log(getNumber());
+        console.timeEnd("getNumber");
+    };
+
+    $('[data-get-number]').on('click', logNumber);
+
+    var logNumberMemoized = _.memoize(logNumber);
+    $('[data-get-number-memoized]').on('click', logNumberMemoized); // its only run once? I dont understand it, sorry.
 
 })();
