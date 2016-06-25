5bbc5f0190a3ccef2e9fe2fece0efc30c995967e
Henrik <henrik_mathiesen@hotmail.com>
2016-06-25 16:06:33 +0200

    -isEqual

--- tst/05-chaining-and-objects/05-chaining-and-objects-isequal.test.js
+++ tst/05-chaining-and-objects/05-chaining-and-objects-isequal.test.js
@@ -10,6 +10,24 @@ Performs an optimized deep comparison between the two objects, to determine if t
 
 describe("05-chaining-and-objects-isequal", function(){
 
+    it("should return true if an object is equal", function(){
 
+        var objOne = {
+            name: "Adam"
+        };
+
+        var objTwo = {
+            name: "Adam"
+        };
+
+        expect(objOne == objTwo).toBe(false);
+        expect(objOne === objTwo).toBe(false);
+        expect(objOne).not.toBe(objTwo);
+        expect(objOne).toEqual(objTwo);
+
+        // allthough they dont reference the same space in memory, they have the same properties and values and are therefor considered equal by _.isEqual()
+        expect(_.isEqual(objOne, objTwo)).toBe(true);
+
+    });
 
 });
