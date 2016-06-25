0086945a407c3c17c3b08d56a8220ced40b930ab
Henrik <henrik_mathiesen@hotmail.com>
2016-06-25 16:30:18 +0200

    -clone

--- tst/05-chaining-and-objects/05-chaining-and-objects-clone.test.js
+++ tst/05-chaining-and-objects/05-chaining-and-objects-clone.test.js
@@ -10,6 +10,44 @@ Create a shallow-copied clone of the provided plain object. Any nested objects o
 
 describe("05-chaining-and-objects-clone", function(){
 
+    it("should create a clone of an object, not a variable holding the same reference", function(){
+
+        var myObjOne = {
+            name: "Henrik"
+        };
+
+        var myObjTwo = myObjOne;
+
+        // These two tests passes because the variables point to the same object in memory
+        expect(myObjOne).toEqual(myObjTwo);
+        expect(myObjOne).toBe(myObjTwo);
+
+        var myClonedObj = _.clone(myObjOne);
+        // These two tests passes because toEqual compares object structure - where as toBe checks if the are the same object in memory (and they are not)
+        expect(myObjOne).toEqual(myClonedObj);
+        expect(myObjOne).not.toBe(myClonedObj);
+
+    });
+
+    it("does however copy nested members by refrence", function(){
+
+        var myObj = {
+            name: "Henrik",
+            foo: {
+                bar: "baz"
+            }
+        };
+
+        var myClone = _.clone(myObj);
+
+        myObj.name = "Adam";
+        myObj.foo.bar = "Zorro";
+
+        expect(myClone.name).toBe("Henrik", "shallow property is not changed to Adam");
+        expect(myClone.foo.bar).toBe("Zorro", "nested property is changed from baz to Zorro");
+
+
+    });
 
 
 });
