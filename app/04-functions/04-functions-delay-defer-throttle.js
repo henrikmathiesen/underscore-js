c79d2ce896f00c00c1fa5b3d3e7f50696bd9b84f
Henrik <henrik_mathiesen@hotmail.com>
2016-06-25 09:51:47 +0200

    -delay,defer,throttle

--- app/04-functions/04-functions-delay-defer-throttle.js
+++ app/04-functions/04-functions-delay-defer-throttle.js
@@ -1,7 +1,19 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
-(function(){
+(function () {
 
+    _.delay(function (args) {
+        console.log("Iam delayed 250 ms " + args);
+    }, 250, "foo");
 
+    console.log("que 1");
+    _.defer(function (args) {
+        console.log("I run when callstack is complete (except timeout apperantly) " + args);
+    }, "bar");
+    console.log("que 2");
+
+    $(window).on('resize', _.throttle(function () {
+        console.log("Iam throttled to run every 1000 ms");
+    }, 1000));
 
 })();
