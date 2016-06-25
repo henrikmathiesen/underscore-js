59a75111780347665db2fd64a8de5f121912baf0
Henrik <henrik_mathiesen@hotmail.com>
2016-06-25 09:17:50 +0200

    -debounce

--- app/04-functions/04-functions-debounce.js
+++ app/04-functions/04-functions-debounce.js
@@ -2,6 +2,19 @@
 
 (function(){
 
+    // Without debounce, event is fired very frequently
+    // $(window).on('resize', function(){
+    //     console.log("resize");
+    // });
 
+    // With debounce it fires when event stops and is not started for n milliseconds, pass true for it to run when event starts
+    $(window).on('resize', _.debounce(function(){
+        console.log("resize");
+    }, 250)); 
+
+    // debounce first (send true as parameter) is a good way to stop accedently submit form clicks
+    $('[data-submit-button]').on('click', _.debounce(function(){
+        console.log("submit");
+    }, 250, true));
 
 })();
