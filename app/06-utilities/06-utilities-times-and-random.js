dcc5b3072eb92fa58f3c78cf1b373d8264705c21
Henrik <henrik_mathiesen@hotmail.com>
2016-07-01 20:13:37 +0200

    -labbing on

--- app/06-utilities/06-utilities-times-and-random.js
+++ app/06-utilities/06-utilities-times-and-random.js
@@ -1,7 +1,16 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
-(function(){
+(function () {
 
-    
+    // Just like a for loop
+    _.times(10, function (index) {
+        console.log(index);
+    });
+
+    // max number is two (random number is 0-2 (yes including 2))
+    console.log(_.random(2));
+
+    // min number is 10 and max number is 12 (random number is 10-12)
+    console.log(_.random(10, 12));
 
 })();
