55fa59d16e8b695428674fe7e70c2ed72c4d08db
Henrik <henrik_mathiesen@hotmail.com>
2016-07-01 18:17:26 +0200

    -labbing on

--- app/06-utilities/06-utilities-mixin.js
+++ app/06-utilities/06-utilities-mixin.js
@@ -1,7 +1,15 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
-(function(){
+(function () {
 
-    
+    _.mixin({
+        getLastIndex: function (collection) {
+            return collection.length - 1;
+        }
+    });
+
+    var myArray = ["Adam", "Bertil"];
+
+    console.log(_.getLastIndex(myArray));
 
 })();
--- app/06-utilities/06-utilities-no-conflict.js
+++ app/06-utilities/06-utilities-no-conflict.js
@@ -2,6 +2,7 @@
 
 (function(){
 
+    // u is now underscore and _ is returned to prev lib owner
     var u = _.noConflict();
     
     var someThings = ["A", "B", "C"];
