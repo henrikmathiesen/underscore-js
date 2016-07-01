02b353637800d7aaf3d7a82aadbe7158d86fcb4a
Henrik <henrik_mathiesen@hotmail.com>
2016-07-01 18:12:21 +0200

    -labbing on

--- app/06-utilities/06-utilities-escape-and-unescape.js
+++ app/06-utilities/06-utilities-escape-and-unescape.js
@@ -1,7 +1,15 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
-(function(){
+(function () {
 
-    
+    $('[data-html-input-raw]').on('keyup', function () {
+        $('[data-html-output-raw]').html($(this).val());
+    });
+
+    $('[data-html-input-escaped]').on('keyup', function(){
+        $('[data-html-output-escaped]').html(_.escape($(this).val()));
+    });
+
+    $('[data-html-output-unescaped]').html(_.unescape("&lt;h2&gt;hi&lt;/h2&gt;"));
 
 })();
