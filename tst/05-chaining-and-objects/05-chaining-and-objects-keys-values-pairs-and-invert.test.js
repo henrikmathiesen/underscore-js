65e1ca62f9f4d3f2be7ea423a454c5ca123f7434
Henrik <henrik_mathiesen@hotmail.com>
2016-07-02 09:24:44 +0200

    -labbing on

--- tst/05-chaining-and-objects/05-chaining-and-objects-keys-values-pairs-and-invert.test.js
+++ tst/05-chaining-and-objects/05-chaining-and-objects-keys-values-pairs-and-invert.test.js
@@ -22,6 +22,30 @@
 
 describe("05-chaining-and-objects-keys-values-pairs-and-invert", function () {
 
+    it("should retrieve all the keys from an object", function () {
 
+        var myObj = { name: "Adam" };
+
+        expect(_.keys(myObj)).toEqual(['name']);
+
+    });
+
+    it("should retrieve all the values from an object", function () {
+        var myObj = { name: "Adam" };
+
+        expect(_.values(myObj)).toEqual(['Adam']);
+    });
+
+    it("should convert an object into a list of [key, value] pairs", function () {
+        var myObj = { name: "Adam" };
+
+        expect(_.pairs(myObj)).toEqual([['name', 'Adam']]);
+    });
+
+    it("should return a copy of the object where the keys have become the values and the values the keys", function () {
+        var myObj = { name: "Adam" };
+
+        expect(_.invert(myObj)).toEqual({ Adam: "name" });
+    });
 
 });
